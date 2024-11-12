1d938b665b104b55793bc5dea4b5ef957bcd1038
Evgeniy <86552627+JonySven@users.noreply.github.com>
2024-11-12 22:44:05 +0300

    верстка (#1)
    Co-authored-by: Imanov <e.imanov@solarl.ru>

--- src/app/features/auth/features/sign-in/sign-in.component.ts
+++ src/app/features/auth/features/sign-in/sign-in.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'ss-sign-in',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, FloatLabelModule, FormsModule, PasswordModule, ReactiveFormsModule, ButtonModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
-export class SignInComponent {}
+export class SignInComponent implements OnInit{
+  formSingIn: FormGroup
+
+  ngOnInit(): void {
+    this.formSingIn = new FormGroup({
+      email: new FormControl('',[Validators.email]),
+      password: new FormControl()
+    })
+  }
+
+  submit(){
+    console.log(this.formSingIn.value);
+}
+}
--- src/app/layouts/components/footer/footer.component.ts
+++ src/app/layouts/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'ss-footer',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
