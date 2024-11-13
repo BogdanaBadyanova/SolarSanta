418925e57e4ab51b6983d3a5f7a9b910238fcc2d
Imanov <e.imanov@solarl.ru>
2024-11-13 21:09:07 +0300

    адаптив

--- src/app/layouts/components/menu/menu.component.ts
+++ src/app/layouts/components/menu/menu.component.ts
@@ -1,8 +1,8 @@
 import { ICurrentUser } from '@/app/core/interfaces/icurrent-user';
 import { UrlsService } from '@/app/core/services/urls.service';
-import { NgTemplateOutlet } from '@angular/common';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
 import { Component, inject, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { SvgIconComponent } from 'angular-svg-icon';
@@ -11,12 +11,28 @@ import { SvgIconComponent } from 'angular-svg-icon';
 @Component({
   selector: 'ss-menu',
   standalone: true,
-  imports: [NgTemplateOutlet, ButtonModule, AvatarModule, RouterLink, SvgIconComponent],
+  imports: [NgTemplateOutlet, ButtonModule, AvatarModule, RouterLink, SvgIconComponent, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
   public urls = inject(UrlsService);
+  private _router: Router
+
+  public isVisibleMenu = false;
 
   public currentUser = input.required<ICurrentUser>();
-}
+
+  public openMenu() {   
+    this.isVisibleMenu = !this.isVisibleMenu
+  }
+
+  public closeMenu() {    
+    this.isVisibleMenu = !this.isVisibleMenu
+  }
+
+  public toRedirect() {
+    this.isVisibleMenu = false
+    this._router.navigate(["/intro"]);
+  }
+ }
