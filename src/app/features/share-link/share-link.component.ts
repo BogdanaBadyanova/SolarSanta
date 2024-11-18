2befbc74a0c22dfb09cfeb0a2cf4ebd318b47261
Imanov <e.imanov@solarl.ru>
2024-11-18 15:30:29 +0300

    стр поделиться

--- src/app/features/share-link/share-link.component.ts
+++ src/app/features/share-link/share-link.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @Component({
   selector: 'ss-share-link',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, FormsModule, InputTextareaModule, SvgIconComponent],
   templateUrl: './share-link.component.html',
   styleUrl: './share-link.component.scss',
 })
-export class ShareLinkComponent {}
+export class ShareLinkComponent {
+  public link = 'qweqwasfdza/fasdfasdaf'
+  public message = 'qweqwasfrewrew23423542352352323dza/fasdfasdaf'
+
+  public sendLink() {
+    console.log('123');
+
+  }
+}
