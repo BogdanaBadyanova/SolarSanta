fd2c4893ad46a34e9835297b009c8e26726b244c
b.badyanova <b.badyanova@solarlab.ru>
2024-11-18 22:09:50 +0300

    Подвал

--- src/app/features/intro/intro.component.ts
+++ src/app/features/intro/intro.component.ts
@@ -1,4 +1,3 @@
-import { FooterComponent } from '@/app/layouts/components/footer/footer.component';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
@@ -7,7 +6,7 @@ import { Urls } from '../../core/utils/urls';
 @Component({
   selector: 'ss-intro',
   standalone: true,
-  imports: [ButtonModule, FooterComponent, RouterLink],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss',
 })
